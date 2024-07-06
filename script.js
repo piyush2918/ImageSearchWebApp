c8ca5fd763bd9a07089d5f8c8102e5b6dc73800c
piyush2918 <piyushkushwaha910@gmail.com>
2024-07-07 03:18:30 +0530

    Added Show more button and refactoring css

--- script.js
+++ script.js
@@ -11,11 +11,14 @@ let page = 1;
 
 async function searchImages(){
     keyword = searchBox.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 
     const response = await fetch(url);
     const data = await response.json();
  
+    if(page===1) {
+        searchResult.innerHTML = "";
+    }    
 
     const results = data.results;
     console.log(results); 
@@ -29,10 +32,16 @@ async function searchImages(){
         imageLink.appendChild(image);
         searchResult.appendChild(imageLink);
     })
+    showMoreBtn.style.display = "block";
 }
 
 searchForm.addEventListener("submit" ,(e)=>{
     e.preventDefault();
     page = 1;
     searchImages(); 
+});
+
+showMoreBtn.addEventListener("click",()=>{
+page++;
+searchImages();
 })
